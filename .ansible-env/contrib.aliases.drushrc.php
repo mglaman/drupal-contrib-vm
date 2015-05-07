f593a458a6c5e61151c468626abcf378530b6584
Matt Glaman <nmd.matt@gmail.com>
2015-05-07 16:50:47 -0500

    Fix aliases.

--- .ansible-env/contrib.aliases.drushrc.php
+++ .ansible-env/contrib.aliases.drushrc.php
@@ -6,78 +6,17 @@
 // Home directory, used to get Vagrant insecure private key.
 $home = drush_server_home();
 
-$aliases['vih'] = array(
-  'uri' => 'vih.clients.dev',
-  'remote-host' => '10.22.22.20',
+$aliases['commerce'] = array(
+  'uri' => 'commerce.contrib.dev',
+  'remote-host' => '10.22.22.10',
   'remote-user' => 'vagrant',
   'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/vih.dk',
+  'root' => '/var/www/commerce',
 );
-$aliases['vies'] = array(
-  'uri' => 'vies.clients.dev',
-  'remote-host' => '10.22.22.20',
+$aliases['panelsverse'] = array(
+  'uri' => 'panelsverse.clients.dev',
+  'remote-host' => '10.22.22.10',
   'remote-user' => 'vagrant',
   'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/vies.dk',
-);
-$aliases['elev'] = array(
-  'uri' => 'elevforening.clients.dev',
-  'remote-host' => '10.22.22.20',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/elevforening.vih.dk',
-);
-$aliases['fond'] = array(
-  'uri' => 'fond.clients.dev',
-  'remote-host' => '10.22.22.20',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/fond.vih.dk',
-);
-$aliases['kursuscenter'] = array(
-  'uri' => 'kursuscenter.clients.dev',
-  'remote-host' => '10.22.22.20',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/kursuscenter.vih.dk',
-);
-$aliases['bhd'] = array(
-  'uri' => 'bhd.clients.dev',
-  'remote-host' => '10.22.22.20',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/bluehorndigital.com',
-);
-$aliases['kntv'] = array(
-  'uri' => 'kntv.clients.dev',
-  'remote-host' => '10.22.22.20',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/kntv',
-);
-$aliases['kntv.remote'] = array(
-  'uri' => 'kntv.loudonco.com',
-  'remote-host' => '104.236.158.44',
-  'root' => '/var/www/kntv',
-);
-$aliases['iuc'] = array(
-  'uri' => 'iuc.clients.dev',
-  'remote-host' => '10.22.22.20',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/iuc',
-);
-$aliases['brx'] = array(
-  'uri' => 'brx.clients.dev',
-  'remote-host' => '10.22.22.20',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/beautyrx/www',
-);
-$aliases['discimport'] = array(
-  'uri' => 'discimport.clients.dev',
-  'remote-host' => '10.22.22.20',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-p 22 -i ' . $home . '/.vagrant.d/insecure_private_key',
-  'root' => '/var/www/discimport',
+  'root' => '/var/www/panelsverse',
 );
